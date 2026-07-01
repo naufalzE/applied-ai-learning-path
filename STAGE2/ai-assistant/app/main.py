71b0ae78e2f55b18935d0d8008cdaf352b637e0a
naufal <naufalzgt05@gmail.com>
2026-07-02 02:24:22 +0700

    add code

--- STAGE2/ai-assistant/app/main.py
+++ STAGE2/ai-assistant/app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 
+from app.core.config import settings
 from app.routers.summarize import router as summarize_router
 
-app = FastAPI()
+
+app = FastAPI(
+    title=settings.app_name
+)
+
 
 app.include_router(summarize_router)
--- STAGE2/ai-assistant/app/routers/summarize.py
+++ STAGE2/ai-assistant/app/routers/summarize.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
+from app.schemas.summarize_schema import SummarizeRequest
 from app.services.summarize_service import SummarizeService
+
 
 router = APIRouter()
 
@@ -8,14 +10,20 @@ service = SummarizeService()
 
 
 @router.post("/summarize")
-def post_summarize(request : SummarizeService):
-    result = service.generate_summary(request.text)
+def post_summarize(request: SummarizeRequest):
+
+    result = service.generate_summary(
+        request.text
+    )
 
     return {
         "summary": result
     }
+
+
 @router.get("/health")
 def get_health():
+
     return {
         "status": "OK"
     }
